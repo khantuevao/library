ed0f5a22152fde50891ca4e32d43bb9264526faa
khantuevao <khantuevao@gmail.com>
2022-06-15 17:32:05 +0200

    Make remove button remove element from DOM & array

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ let theHobbit = {
   title: 'The Hobbit',
   author: 'J.R.R. Tolkien',
   pages: 310,
-  status: 'Not read'
+  status: 'Not read',
 }
 
 let book1984 = {
   title: '1984',
   author: 'G. Orwell',
   pages: 328,
-  status: 'Read'
+  status: 'Read',
 }
 
 let warPeace = {
   title: 'War and Peace',
   author: 'L. Tolstoy',
   pages: 1225,
-  status: 'Read'
+  status: 'Read',
 }
 
 let myLibrary = [theHobbit, book1984, warPeace];
@@ -36,23 +36,31 @@ function displayBooks() {
   for (let i = 0; i < myLibrary.length; i++) {
     let bookContainer = document.createElement('div');
     bookContainer.classList.add('book-info');
+    bookContainer.setAttribute('data-index', `${i}`);
+
     let titleDiv = document.createElement('div');
     titleDiv.textContent = `${(myLibrary[i]).title}`;
     bookContainer.appendChild(titleDiv);
+
     let authorDiv = document.createElement('div');
     authorDiv.textContent = `${(myLibrary[i]).author}`;
     bookContainer.appendChild(authorDiv);
+
     let pagesDiv = document.createElement('div');
     pagesDiv.textContent = `${(myLibrary[i]).pages} pages`;
     bookContainer.appendChild(pagesDiv);
+
     let statusButton = document.createElement('button');
     statusButton.classList.add('status-button');
     statusButton.textContent = `${(myLibrary[i]).status}`;
     bookContainer.appendChild(statusButton);
+
     let removeButton = document.createElement('button');
     removeButton.classList.add('remove-button');
     removeButton.textContent = 'Remove';
+    removeButton.setAttribute('data-index', `${i}`);
     bookContainer.appendChild(removeButton);
+
     let mainContainer = document.querySelector('#container');
     mainContainer.appendChild(bookContainer);
   }
@@ -79,4 +87,19 @@ submitButton.addEventListener('click', () => {
   addBookToLibrary(inputBook);
   clearBooks();
   displayBooks();
+  addEvent();
 });
+
+function addEvent() {
+  let btns = document.getElementsByClassName('remove-button');
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', () => {
+      myLibrary.splice(i, 1);
+      clearBooks();
+      displayBooks();
+      addEvent();
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', addEvent);
