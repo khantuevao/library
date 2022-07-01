93ccdaea783f2902d0b39d9fdf1023fef8ef85fa
khantuevao <khantuevao@gmail.com>
2022-07-01 13:13:54 +0200

    Add form validation, input cannot be empty

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let theHobbit = {
-  title: 'The Hobbit',
-  author: 'J.R.R. Tolkien',
+  title: "The Hobbit",
+  author: "J.R.R. Tolkien",
   pages: 310,
-  status: 'Not read',
-}
+  status: "Not read",
+};
 
 let book1984 = {
-  title: '1984',
-  author: 'G. Orwell',
+  title: "1984",
+  author: "G. Orwell",
   pages: 328,
-  status: 'Read',
-}
+  status: "Read",
+};
 
 let warPeace = {
-  title: 'War and Peace',
-  author: 'L. Tolstoy',
+  title: "War and Peace",
+  author: "L. Tolstoy",
   pages: 1225,
-  status: 'Read',
-}
+  status: "Read",
+};
 
 let myLibrary = [theHobbit, book1984, warPeace];
 
@@ -39,53 +39,75 @@ function addBookToLibrary(book) {
 
 function displayBooks() {
   for (let i = 0; i < myLibrary.length; i++) {
-    let bookContainer = document.createElement('div');
-    bookContainer.classList.add('book-info');
+    let bookContainer = document.createElement("div");
+    bookContainer.classList.add("book-info");
 
-    let titleDiv = document.createElement('div');
-    titleDiv.textContent = `${(myLibrary[i]).title}`;
+    let titleDiv = document.createElement("div");
+    titleDiv.textContent = `${myLibrary[i].title}`;
     bookContainer.appendChild(titleDiv);
 
-    let authorDiv = document.createElement('div');
-    authorDiv.textContent = `${(myLibrary[i]).author}`;
+    let authorDiv = document.createElement("div");
+    authorDiv.textContent = `${myLibrary[i].author}`;
     bookContainer.appendChild(authorDiv);
 
-    let pagesDiv = document.createElement('div');
-    pagesDiv.textContent = `${(myLibrary[i]).pages} pages`;
+    let pagesDiv = document.createElement("div");
+    pagesDiv.textContent = `${myLibrary[i].pages} pages`;
     bookContainer.appendChild(pagesDiv);
 
-    let statusButton = document.createElement('button');
-    statusButton.classList.add('status-button');
-    statusButton.textContent = `${(myLibrary[i]).status}`;
+    let statusButton = document.createElement("button");
+    statusButton.classList.add("status-button");
+    statusButton.textContent = `${myLibrary[i].status}`;
     bookContainer.appendChild(statusButton);
 
-    let removeButton = document.createElement('button');
-    removeButton.classList.add('remove-button');
-    removeButton.textContent = 'Remove';
+    let removeButton = document.createElement("button");
+    removeButton.classList.add("remove-button");
+    removeButton.textContent = "Remove";
     bookContainer.appendChild(removeButton);
 
-    let mainContainer = document.querySelector('#container');
+    let mainContainer = document.querySelector("#container");
     mainContainer.appendChild(bookContainer);
   }
 }
 
 function clearBooks() {
-  let bookInfo = document.getElementsByClassName('book-info');
+  let bookInfo = document.getElementsByClassName("book-info");
   for (let i = bookInfo.length - 1; i >= 0; i--) {
     bookInfo[i].remove();
   }
 }
 
-const submitButton = document.querySelector('#submit');
-submitButton.addEventListener('click', () => {
-  let inputTitle = (document.querySelector('#title')).value;
-  let inputAuthor = (document.querySelector('#author')).value;
-  let inputPages = (document.querySelector('#pages')).value;
-  let inputStatus = (document.querySelector('#status')).value;
-  if (inputTitle === '' || inputAuthor === '' || inputPages === '') {
-    alert('You need to fill all the fields');
-    return;
+document.addEventListener("DOMContentLoaded", addEvent);
+
+const addFormValidation = () => {
+  const titleInput = document.getElementById("title");
+  const authorInput = document.getElementById("author");
+  const pagesInput = document.getElementById("pages");
+
+  const validityCheck = (input) => {
+    input.addEventListener('input', () => {
+      input.setCustomValidity('');
+      input.checkValidity();
+    })
+    input.addEventListener('invalid', () => {
+      if(input.value === '') {
+        input.setCustomValidity('cannot be empty');
+      }
+    });
   }
+
+  validityCheck(titleInput);
+  validityCheck(authorInput);
+  validityCheck(pagesInput);
+}
+
+const submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", () => {
+  addFormValidation();
+  let inputTitle = document.querySelector("#title").value;
+  let inputAuthor = document.querySelector("#author").value;
+  let inputPages = document.querySelector("#pages").value;
+  let inputStatus = document.querySelector("#status").value;
+
   let inputBook = new Book(inputTitle, inputAuthor, inputPages, inputStatus);
   addBookToLibrary(inputBook);
   clearBooks();
@@ -94,25 +116,24 @@ submitButton.addEventListener('click', () => {
 });
 
 function addEvent() {
-  let rbtns = document.getElementsByClassName('remove-button');
+  let rbtns = document.getElementsByClassName("remove-button");
   for (let i = 0; i < rbtns.length; i++) {
-    rbtns[i].addEventListener('click', () => {
+    rbtns[i].addEventListener("click", () => {
       myLibrary.splice(i, 1);
       clearBooks();
       displayBooks();
       addEvent();
     });
   }
-  let sbtns = document.getElementsByClassName('status-button');
+  let sbtns = document.getElementsByClassName("status-button");
   for (let i = 0; i < sbtns.length; i++) {
-    sbtns[i].addEventListener('click', () => {
-      (myLibrary[i].status === 'Read')? myLibrary[i].status = 'Not read' : myLibrary[i].status = 'Read';
+    sbtns[i].addEventListener("click", () => {
+      myLibrary[i].status === "Read"
+        ? (myLibrary[i].status = "Not read")
+        : (myLibrary[i].status = "Read");
       clearBooks();
       displayBooks();
       addEvent();
     });
   }
 }
-
-document.addEventListener('DOMContentLoaded', addEvent);
-
